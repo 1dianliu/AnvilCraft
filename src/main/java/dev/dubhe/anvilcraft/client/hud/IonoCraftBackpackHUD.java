c272480a0f7c23a385124816b1a2aafcf4111001
DancingSnow <1121149616@qq.com>
2025-03-02 23:59:01 +0800

    添加电池图标，添加物品图标

--- src/main/java/dev/dubhe/anvilcraft/client/hud/IonoCraftBackpackHUD.java
+++ src/main/java/dev/dubhe/anvilcraft/client/hud/IonoCraftBackpackHUD.java
@@ -10,9 +10,13 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
 public class IonoCraftBackpackHUD {
+    private static final ResourceLocation BATTERY_EMPTY = AnvilCraft.of("widget/battery_display/battery_empty");
+    private static final ResourceLocation BATTERY_FULL = AnvilCraft.of("widget/battery_display/battery_full");
+
     public static void render(GuiGraphics guiGraphics, DeltaTracker partialTick) {
         if (!AnvilCraft.config.ionoCraftBackpackHud.enabled) {
             return;
@@ -36,12 +40,23 @@ public class IonoCraftBackpackHUD {
         int x = AnvilCraft.config.ionoCraftBackpackHud.hudX;
         int y = AnvilCraft.config.ionoCraftBackpackHud.hudY;
         float scale = AnvilCraft.config.ionoCraftBackpackHud.hudScale;
-        poseStack.scale(scale, scale, scale);
 
+        poseStack.scale(scale, scale, scale);
+        poseStack.translate(x, y, 0);
+        guiGraphics.renderItem(itemStack, 0, 0);
+
+        poseStack.translate(20, 4, 0);
         Component text = Component.translatable("hud.anvilcraft.ionocraft_backpack_power", percent);
         int textWidth = font.width(text);
+        guiGraphics.drawString(font, text, 0, 0, 0xFFFFFFFF, true);
 
-        guiGraphics.drawString(font, text, x, y, 0xFFFFFFFF, true);
+        int batteryHeight = (int) (percent / 100f * 16);
+
+        poseStack.translate(textWidth + 4, -4, 0);
+        guiGraphics.blitSprite(BATTERY_EMPTY, 0, 0, 8, 16);
+
+        poseStack.translate(0, 0, 1);
+        guiGraphics.blitSprite(BATTERY_FULL, 8, 16, 0, 16 - batteryHeight, 0, 16 - batteryHeight, 8, batteryHeight);
 
         poseStack.popPose();
     }
